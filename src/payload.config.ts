26c9b73d0d30595baa19b4032e93b88a3fea1da2
Sandro Wegmann <sandro.wegmann@10xmedia.de>
2025-11-18 15:16:18 +0800

    init

---
+++ src/payload.config.ts
@@ -0,0 +1,41 @@
+// storage-adapter-import-placeholder
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import path from 'path'
+import { buildConfig } from 'payload'
+import { fileURLToPath } from 'url'
+import sharp from 'sharp'
+
+import { Users } from './collections/Users'
+import { Media } from './collections/Media'
+import { resendAdapter } from '@payloadcms/email-resend'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    importMap: {
+      baseDir: path.resolve(dirname),
+    },
+  },
+  email: resendAdapter({
+    defaultFromAddress: 'noreply@ns-payload-basics.10xmedia.de',
+    defaultFromName: 'Sandro Wegmann',
+    apiKey: process.env.RESEND_API_KEY || '',
+  }),
+  collections: [Users, Media],
+  editor: lexicalEditor(),
+  secret: process.env.PAYLOAD_SECRET || '',
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI || '',
+  }),
+  sharp,
+  plugins: [
+    // storage-adapter-placeholder
+  ],
+})
